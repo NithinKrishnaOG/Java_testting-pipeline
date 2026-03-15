d65b1723801590b3601589bd21d4eae603b5b886
NithinKrishnaOG <nithinkrishnaog@gmail.com>
2026-03-15 16:29:00 +0000

    dinr

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
     @GetMapping("/api/hello")
     public String hello() {
-        return "Hello from the CI/CD Pipeline! v2";
+        return "Hello from the CI/CD Pipeline! v3";
     }
 }
